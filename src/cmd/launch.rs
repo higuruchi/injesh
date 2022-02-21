87144e1ee5a694348b286f224375e015413bf1b3
higuruchi <fumiya2324@gmail.com>
2022-02-21 12:16:16 +0000

    launchのエラー型を修正

--- src/cmd/launch.rs
+++ src/cmd/launch.rs
@@ -4,8 +4,9 @@ use crate::command;
 pub struct LaunchStruct;
 
 impl Launch for LaunchStruct {
-    fn launch(&self, launch: &command::Launch) {
+    fn launch(&self, launch: &command::Launch)  -> Result<(), Box<dyn std::error::Error>> {
         println!("execute launch!");
+        Ok(())
     }
 }
 
