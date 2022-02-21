ad8375f3e939bb4c3cb5d7664d614cb45c733dd9
yassi-github <s19t312@kagawa-u.ac.jp>
2022-02-21 01:03:40 +0000

    implement list command

--- src/cmd/list.rs
+++ src/cmd/list.rs
@@ -1,10 +1,36 @@
 use crate::list::List;
+use std::{env, fs};
 
 pub struct ListStruct;
 
 impl List for ListStruct {
     fn list(&self) {
-        println!("execute list!");
+        // println!("execute list!");
+        let homedir = match env::var("HOME") {
+            Ok(path) => path,
+            Err(_) => String::new(),
+        };
+        let containers_root = format!("{HOME}/.injesh/containers", HOME = homedir);
+        // debug
+        // let containers_root = format!("{}/target", env::var("PWD").unwrap());
+        let mut container_names = String::new();
+
+        match fs::read_dir(containers_root) {
+            Ok(paths) => {
+                for path in paths.flatten() {
+                    if path.path().is_dir() {
+                        let container_name = match path.file_name().into_string() {
+                            Ok(name) => name,
+                            Err(_) => String::from("<none>"),
+                        };
+                        container_names += &format!("{}\n", container_name);
+                    }
+                }
+            }
+            Err(why) => eprintln!("{:?}", why.kind()),
+        }
+
+        println!("{}", container_names.trim());
     }
 }
 
