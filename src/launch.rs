5dbce4652aa1ea18c10859d5a8f44b3c4b76f557
higuruchi <fumiya2324@gmail.com>
2022-07-11 18:09:52 +0000

    不必要なファイルを削除

--- src/launch.rs
+++
@@ -1,9 +0,0 @@
-use crate::image_downloader::Downloader;
-use crate::{command, setting};
-
-pub trait Launch<DO>
-where
-    DO: Downloader,
-{
-    fn launch(&self, launch: &mut command::Launch<DO>) -> Result<(), Box<dyn std::error::Error>>;
-}
