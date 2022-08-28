a2cd464b3721c7236da68db6b37c3d01ae93a85e
higuruchi <fumiya2324@gmail.com>
2022-08-28 06:48:48 +0000

    delete: 削除できないバグを修正

--- src/cmd/common.rs
+++ src/cmd/common.rs
@@ -1,5 +1,33 @@
-use nix::unistd::{Gid, Uid};
-use std::fs::OpenOptions;
+use crate::container;
+
+use nix::{
+    mount::{mount, MsFlags},
+    unistd::{Gid, Uid},
+};
+use std::{
+    error, fmt,
+    fs::{copy, create_dir, read_dir, OpenOptions},
+    path::{Path, PathBuf},
+};
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidPath(PathBuf),
+    OvarlayfsDirInvalid,
+    MountFailed(nix::errno::Errno),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::InvalidPath(path) => write!(f, "cmd::common::InvalidPath: {:?}", path),
+            Error::OvarlayfsDirInvalid => write!(f, "cmd::common::OvarlayfsDirInvalid"),
+            Error::MountFailed(why) => write!(f, "cmd::command::MountFailed: because of {}", why),
+        }
+    }
+}
+
+impl error::Error for Error {}
 
 pub fn new_uidmap(uid: &Uid) -> Result<(), Box<dyn std::error::Error>> {
     use std::ffi::CString;
@@ -28,6 +56,92 @@ pub fn new_gidmap(gid: &Gid) -> Result<(), Box<dyn std::error::Error>> {
     let gidmap = CString::new(format!("0 {} 1", gid.as_raw()))?;
 
     gidmap_file.write(gidmap.as_bytes())?;
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn copy_dir_recursively(src: &PathBuf, dest: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    let src_pathbuf = src.clone();
+    let dest_pathbuf = dest.clone();
+    let src_path = src.as_path();
+    let dest_path = dest.as_path();
+
+    if !src_path.is_dir() {
+        Err(Error::InvalidPath(src_pathbuf))?
+    }
+    if !dest_path.is_dir() {
+        Err(Error::InvalidPath(dest_pathbuf))?
+    }
+
+    for entry_result in read_dir(src)? {
+        let entry = entry_result?;
+        let entry_path = entry.path();
+        let dest_entry_path = dest_path.join(
+            &entry_path
+                .file_name()
+                .ok_or(Error::InvalidPath(entry_path.clone()))?,
+        );
+
+        if entry.file_type()?.is_dir() {
+            create_dir(&dest_path)?;
+            copy_dir_recursively(&entry_path, &dest_entry_path)?;
+        } else {
+            copy(entry_path, &dest_path)?;
+        }
+    }
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn overwrite_target_upperdir_by_own_upperdir(
+    container_dir_path: &Path,
+    overlayfs_dirs: &container::Container,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let target_upperdir = overlayfs_dirs.upperdir();
+    let own_upperdir = container_dir_path.join("upper");
+
+    copy_dir_recursively(&own_upperdir, target_upperdir)?;
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn mount_merged_directory(
+    overlayfs_dirs: &container::Container,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let lowerdir_string = overlayfs_dirs
+        .lowerdir()
+        .clone()
+        .into_os_string()
+        .into_string()
+        .map_err(|_| Error::OvarlayfsDirInvalid)?;
+    let upperdir_string = overlayfs_dirs
+        .upperdir()
+        .clone()
+        .into_os_string()
+        .into_string()
+        .map_err(|_| Error::OvarlayfsDirInvalid)?;
+    let workdir_string = overlayfs_dirs
+        .workdir()
+        .clone()
+        .into_os_string()
+        .into_string()
+        .map_err(|_| Error::OvarlayfsDirInvalid)?;
+    mount(
+        Some("overlay"),
+        overlayfs_dirs.mergeddir(),
+        Some("overlay"),
+        MsFlags::empty(),
+        Some(
+            format!(
+                "lowerdir={},upperdir={},workdir={}",
+                lowerdir_string, upperdir_string, workdir_string
+            )
+            .as_str(),
+        ),
+    )
+    .map_err(|why| Error::MountFailed(why))?;
 
     Ok(())
 }
--- src/cmd/delete.rs
+++ src/cmd/delete.rs
@@ -1,9 +1,9 @@
-use crate::command::{self, delete_error::Error};
-use crate::container;
-use crate::user;
-use crate::utils;
+use crate::{
+    command::{self, delete_error::Error},
+    user, utils,
+};
 
-use nix::mount::{mount, umount2, MntFlags, MsFlags};
+use nix::mount::{umount2, MntFlags};
 use std::{fs, path};
 
 pub struct DeleteStruct;
@@ -19,29 +19,15 @@ impl DeleteStruct {
             user::User::new()?.containers(),
             injesh_container_name
         );
+        let container_merged_dir_path = format!("{}/merged", container_dir_path);
         let container_dir_path = path::Path::new(&container_dir_path);
+        let container_merged_dir_path = path::Path::new(&container_merged_dir_path);
 
-        // check container is exists
         check_container_exists(container_dir_path)?;
-        let overlayfs_dirs = container::Container::new(injesh_container_name)?;
 
-        // umount merged directory
-        // as syscall: umount2("/path/to/dest", 0)
-        umount2(overlayfs_dirs.mergeddir(), MntFlags::empty())
+        umount2(container_merged_dir_path, MntFlags::empty())
             .map_err(|why| Error::UnmountFailed(why))?;
 
-        // restore original upperdir
-        overwrite_target_upperdir_by_own_upperdir(container_dir_path, &overlayfs_dirs)?;
-
-        // mount merged directory
-        mount_merged_directory(&overlayfs_dirs)?;
-
-        // restart container
-        let docker_container_id =
-            container::Container::convert_injesh_name_to_docker_id(injesh_container_name)?;
-        container::Container::new(&docker_container_id)?.restart()?;
-
-        // delete own containers directory
         fs::remove_dir_all(container_dir_path).map_err(|why| Error::RemoveFailed(why))?;
 
         Ok(())
@@ -59,92 +45,5 @@ fn check_container_exists(
         Err(Error::ContainerNotFound)?
     }
 
-    Ok(())
-}
-
-fn overwrite_target_upperdir_by_own_upperdir(
-    container_dir_path: &path::Path,
-    overlayfs_dirs: &container::Container,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let target_upperdir = overlayfs_dirs.upperdir();
-    let own_upperdir = container_dir_path.join("upper");
-
-    // copy_dir_recursively(&own_upperdir, target_upperdir).map_err(|why| Error::CopyFailed(why))?;
-    copy_dir_recursively(&own_upperdir, target_upperdir)?;
-
-    Ok(())
-}
-
-fn mount_merged_directory(
-    overlayfs_dirs: &container::Container,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let lowerdir_string = overlayfs_dirs
-        .lowerdir()
-        .clone()
-        .into_os_string()
-        .into_string()
-        .map_err(|_| Error::OvarlayfsDirInvalid)?;
-    let upperdir_string = overlayfs_dirs
-        .upperdir()
-        .clone()
-        .into_os_string()
-        .into_string()
-        .map_err(|_| Error::OvarlayfsDirInvalid)?;
-    let workdir_string = overlayfs_dirs
-        .workdir()
-        .clone()
-        .into_os_string()
-        .into_string()
-        .map_err(|_| Error::OvarlayfsDirInvalid)?;
-    mount(
-        Some("overlay"),
-        overlayfs_dirs.mergeddir(),
-        Some("overlay"),
-        MsFlags::empty(),
-        Some(
-            format!(
-                "lowerdir={},upperdir={},workdir={}",
-                lowerdir_string, upperdir_string, workdir_string
-            )
-            .as_str(),
-        ),
-    )
-    .map_err(|why| Error::MountFailed(why))?;
-
-    Ok(())
-}
-
-fn copy_dir_recursively(
-    src: &std::path::PathBuf,
-    dest: &std::path::PathBuf,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let src_pathbuf = src.clone();
-    let dest_pathbuf = dest.clone();
-    let src_path = src.as_path();
-    let dest_path = dest.as_path();
-
-    if !src_path.is_dir() {
-        Err(Error::InvalidPath(src_pathbuf))?
-    }
-    if !dest_path.is_dir() {
-        Err(Error::InvalidPath(dest_pathbuf))?
-    }
-
-    for entry_result in fs::read_dir(src)? {
-        let entry = entry_result?;
-        let entry_path = entry.path();
-        let dest_entry_path = dest_path.join(
-            &entry_path
-                .file_name()
-                .ok_or(Error::InvalidPath(entry_path.clone()))?,
-        );
-
-        if entry.file_type()?.is_dir() {
-            fs::create_dir(&dest_path)?;
-            copy_dir_recursively(&entry_path, &dest_entry_path)?;
-        } else {
-            fs::copy(entry_path, &dest_path)?;
-        }
-    }
     Ok(())
 }
